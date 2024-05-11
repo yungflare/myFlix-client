d83cab754e52369397ae23777f10a90309c588cf
Daniela Codes <daniela.antonio.430@gmail.com>
2024-05-10 21:20:00 -0400

    Testing Filter Movie

--- src/components/SearchBar/search-filter.jsx
+++ src/components/SearchBar/search-filter.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Form, Col } from "react-bootstrap";
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Col className="mb-3">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Control
             type="text"
